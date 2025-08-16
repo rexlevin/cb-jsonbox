4e9ad70d952b3c12d4264d9cff5bea7a7cca68fd
lizl6 <broodmantis@163.com>
2025-08-16 11:30:08 +0800

    引入element-plus

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router/router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(router).mount('#app')
